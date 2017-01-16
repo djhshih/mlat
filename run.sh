6fd8aef1eec35e4c350adef71a13731b723145ef
David J. H. Shih <djh.shih@gmail.com>
2017-01-15 22:49:33 -0500

    Added more examples.

--- run.sh
+++ run.sh
@@ -4,8 +4,12 @@
 
 ./mlat database query test.psl || true
 
-./mlat data/hg38_tp53.2bit data/reads_tp53.fa test.psl
-./mlat data/hg38_tp53.2bit data/reads_tp53.fa.gz test.psl
+./mlat data/ref1.fna data/query1.fna test0.psl
+./mlat -mask=lower data/ref1.fna data/query1.fna test0.psl
+./mlat -trimT -trimHardA data/ref1.fna data/query1.fna test0.psl
+
+./mlat data/hg38_tp53.2bit data/reads_tp53.fa test1.psl
+./mlat data/hg38_tp53.2bit data/reads_tp53.fa.gz test1.psl
 
 ./mlat -t=prot -q=prot data/protein_tp53.faa data/peptides_tp53.faa test2.psl
 
