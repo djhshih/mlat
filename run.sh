b6af69fd9f09c7b4eb35d81474a65836f4f2f6a7
David J. H. Shih <djh.shih@gmail.com>
2017-01-16 01:56:19 -0500

    Add different output format tests.

--- run.sh
+++ run.sh
@@ -12,6 +12,11 @@
 ./mlat data/hg38_tp53.2bit data/reads_tp53.fa test1.psl
 ./mlat data/hg38_tp53.2bit data/reads_tp53.fa.gz test1.psl
 
+formats=( pslx axt maf sim4 wublast blast blast8 blast9 )
+for format in ${formats[@]}; do
+	./mlat -out=${format} data/hg38_tp53.2bit data/reads_tp53.fa.gz test1.${format}
+done
+
 ./mlat -t=dnax -q=dnax data/hg38_tp53.2bit data/reads_tp53.fa.gz test2.psl
 
 ./mlat -t=prot -q=prot data/protein_tp53.faa data/peptides_tp53.faa test3.psl
