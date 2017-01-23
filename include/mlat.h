26ea55787be105c55af4c08125253c9bf4f04494
David J. H. Shih <djh.shih@gmail.com>
2017-01-23 16:37:49 -0500

    Added c extern guard.

--- include/mlat.h
+++ include/mlat.h
@@ -1,4 +1,12 @@
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
 #include "mlat/gfResult.h"
 #include "mlat/aliType.h"
 #include "mlat/mlatParams.h"
 #include "mlat/gfDb.h"
+
+#ifdef __cplusplus
+}
+#endif 
