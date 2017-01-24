a2263778adbd05bf7251dbf742b606a4c50cecca
David J. H. Shih <djh.shih@gmail.com>
2017-01-24 01:48:46 -0500

    Added mlat function to API.

--- include/mlat.h
+++ include/mlat.h
@@ -7,6 +7,9 @@ extern "C"{
 #include "mlat/mlatParams.h"
 #include "mlat/gfDb.h"
 
+/* minimal BLAT fast sequence alignment tool */
+void mlat(char *dbFile, char *queryFile, char *outName, struct mlatParams *p);
+
 #ifdef __cplusplus
 }
 #endif 
