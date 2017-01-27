400884843c9f00a8d43f6f0ec72543cfee76b8c2
David J. H. Shih <djh.shih@gmail.com>
2017-01-27 17:21:25 -0500

    Fix compiler errors with gcc on Linux.

--- common.mk
+++ common.mk
@@ -1,7 +1,14 @@
-COPTS=-I$(ROOT)/include -I$(ROOT)/include/private/core -I$(ROOT)/include/private/aux -I$(ROOT)/include/private/net
-CFLAGS=-O3 -Os
-#CFLAGS=-g
-#CFLAGS=-coverage -O0
+MACHTYPE?=$(shell uname -m)
+
+COPTS=-I$(ROOT)/include -I$(ROOT)/include/private/core -I$(ROOT)/include/private/aux -I$(ROOT)/include/private/net -DMACHTYPE_${MACHTYPE}
+
+CFLAGS_RELEASE=-O3 -Os
+CFLAGS_DEBUG=-g
+CFLAGS_COV=-coverage -O0
+# disable fortify to circumvent missing __memchk in glibc
+CFLAGS_STATIC=-O2 -Os -static -static-libgcc -D_STATIC -D_FORTIFY_SOURCE=0
+
+CFLAGS=$(CFLAGS_RELEASE)
 
 CBUILD=$(CC) $(COPTS) $(CFLAGS)
 
